6a1c55e3822105fc5668f4f1f758e884e25779e2
kamaihamaiha <kamaihamaiha@gmail.com>
2017-08-01 18:42:18 +0800

    找到一个免费的天气预报接口，返回的是 XML 数据。

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 
 - 目的是学习 Android 技术
 - 抵制那些加广告的天气预报应用
+- 免费接口参考：http://blog.csdn.net/fancylovejava/article/details/26102635
--- app/src/main/java/com/leslie/weather/MainActivity.java
+++ app/src/main/java/com/leslie/weather/MainActivity.java
@@ -2,12 +2,79 @@ package com.leslie.weather;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.io.IOException;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import okhttp3.FormBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class MainActivity extends Activity {
+    private static final String TAG = "MainActivity";
+    //----------------------------------------------------------------------------------------------
+    @Bind(R.id.bt_get_weather_info)
+    Button btGetWeatherInfo;
+    @Bind(R.id.tv_weather_info)
+    TextView tvWeatherInfo;
+    //----------------------------------------------------------------------------------------------
+    private OkHttpClient client;
+    private FormBody body;
+    private Request request;
+    //----------------------------------------------------------------------------------------------
+    private static final int WEATHER_DATA_SUCCESS = 1001;
+    //----------------------------------------------------------------------------------------------
+    private Handler mHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case WEATHER_DATA_SUCCESS:
+                    String xmlData = (String) msg.obj;
+                    tvWeatherInfo.setText(xmlData);
+                    //解析 XML 数据
+                    break;
+            }
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+
+    }
+
+    @OnClick(R.id.bt_get_weather_info)
+    public void onViewClicked() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                getDataByOKHttp();
+            }
+        }).start();
+
+    }
+    private void getDataByOKHttp() {
+        try {
+            client = new OkHttpClient();
+            request = new Request.Builder().url("http://wthrcdn.etouch.cn/WeatherApi?city=济南").build();
+            Response response = client.newCall(request).execute();
+            String responseData = response.body().string();
+            Message message = mHandler.obtainMessage();
+            message.obj = responseData;
+            message.what = WEATHER_DATA_SUCCESS;
+            mHandler.sendMessage(message);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
